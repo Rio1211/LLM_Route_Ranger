358040169bc9775f98442ed897a5095076b62039
Rio1211 <memory85432@gmail.com>
2025-11-09 16:41:45 -0700

    folder structre added.

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Create the directory
+mkdir -p src
+mkdir -p research
+
+# Create the files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.py
+touch requirements.txt
+
+echo "All Directories and Files Created!"
