4eb534004fae1c5f4de36afbfb5179e790bb6693
Rio1211 <memory85432@gmail.com>
2025-11-11 21:38:21 -0700

    completed the front-end

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def index():
     return render_template('chat.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port = 8080, debug=True)
+    app.run(host="0.0.0.0", port = 8081, debug=True)
